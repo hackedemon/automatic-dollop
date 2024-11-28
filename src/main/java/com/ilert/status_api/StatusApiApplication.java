d1bfb960a267e449d981d63415b35c7b1ca649a5
Sheetal <sheetalved2991@gmail.com>
2024-11-28 09:15:50 +0100

    Initial commit.

---
+++ src/main/java/com/ilert/status_api/StatusApiApplication.java
@@ -0,0 +1,13 @@
+package com.ilert.status_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilert/status_api/StatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilert.status_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
